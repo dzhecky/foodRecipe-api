904f667f247cd20410d7ca4491d15e7b528ec47f
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2023-11-01 11:47:30 +0700

    add cors and morgan

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const Router = require('./src/router');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: '*',
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan('combined'));
 app.use(express.json());
 
 app.get('/', (req, res, next) => {
