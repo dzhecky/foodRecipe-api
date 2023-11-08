69e89ac2d874cdb84486670e946765f1a4f6da9c
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2023-11-08 18:52:54 +0700

    fix bug register

--- src/models/auth.js
+++ src/models/auth.js
@@ -1,18 +1,15 @@
 const Pool = require('../config/db');
 
 const createUser = async (data) => {
-  const { name, email, passwordHashed, phone_number, photo, uuid } = data;
+  const { name, email, passwordHashed, uuid } = data;
   return new Promise((resolve, reject) => {
-    Pool.query(
-      `INSERT INTO users (name, email, password, phone_number, photo, created_time, updated_time, uuid) VALUES ('${name}', '${email}', '${passwordHashed}', '${phone_number}', '${photo}', current_timestamp, current_timestamp, '${uuid}')`,
-      (err, result) => {
-        if (!err) {
-          return resolve(result);
-        } else {
-          return reject(err);
-        }
+    Pool.query(`INSERT INTO users (name, email, password, created_time, updated_time, uuid) VALUES ('${name}', '${email}', '${passwordHashed}', current_timestamp, current_timestamp, '${uuid}')`, (err, result) => {
+      if (!err) {
+        return resolve(result);
+      } else {
+        return reject(err);
       }
-    );
+    });
   });
 };
 
--- src/router/auth.js
+++ src/router/auth.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const { register, setActivateUser, login, refreshToken } = require('../controllers/auth');
-const upload = require('../middleware/upload');
 
 const router = express.Router();
 
-router.post('/register', upload.single('photo'), register);
+router.post('/register', register);
 router.get('/activate/:id', setActivateUser);
 router.post('/login', login);
 router.post('/refresh', refreshToken);
