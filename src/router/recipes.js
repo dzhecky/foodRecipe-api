c77ccaecb282c0be309c62a9a29ba5c068a7cf62
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2023-11-08 19:13:22 +0700

    fix bug get recipes and add route delete recipes by admin

--- src/router/recipes.js
+++ src/router/recipes.js
@@ -2,21 +2,24 @@ const express = require('express');
 const { allRecipes, getRecipeById, showRecipeByIdUser, postRecipe, putRecipe, deleteRecipeId, myRecipes } = require('../controllers/recipes');
 const verifyToken = require('../middleware/auth');
 const { isActivated } = require('../middleware/isActivated');
-const { recipeOwner, usersAndAdmin } = require('../middleware/roleUsers');
+const { recipeOwner, usersAndAdmin, onlyAdmin } = require('../middleware/roleUsers');
 const upload = require('../middleware/upload');
 const router = express.Router();
 
 // All role
-router.get('/', verifyToken, isActivated, allRecipes);
-router.get('/detail/:id', verifyToken, isActivated, getRecipeById);
+router.get('/', allRecipes);
+router.get('/detail/:id', getRecipeById);
 router.get('/:id', verifyToken, isActivated, showRecipeByIdUser);
 
 // Only users and admin
 router.post('/', verifyToken, isActivated, usersAndAdmin, upload.single('photo'), postRecipe);
 router.get('/show/myrecipes', verifyToken, isActivated, usersAndAdmin, myRecipes);
 
-// Only recipe owner admin
+// Only recipe owner
 router.put('/:id', verifyToken, isActivated, recipeOwner, upload.single('photo'), putRecipe);
 router.delete('/:id', verifyToken, isActivated, recipeOwner, deleteRecipeId);
+
+// delete recipe by admin
+router.delete('/deleteByAdmin/:id', verifyToken, isActivated, onlyAdmin, deleteRecipeId);
 
 module.exports = router;
