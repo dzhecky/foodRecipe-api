17962bf4e02ced77bbcf0804ab22b1e04f2558ce
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2023-11-25 13:40:48 +0700

    add features and route comments recipes

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,13 @@ const users = require('./users');
 const recipes = require('./recipes');
 const category = require('./category');
 const event = require('./event');
+const comments = require('./comments');
 
 router.use('/auth', auth);
 router.use('/users', users);
 router.use('/recipe', recipes);
 router.use('/category', category);
 router.use('/event', event);
+router.use('/comments', comments);
 
 module.exports = router;
